c5b6577a81883221664fb5da0d5f635a120662d0
Matti Nykyri <zonite@iki.fi>
2024-09-26 19:41:16 +0300

    reencode

--- daemon/skripts/reencode.sh
+++ daemon/skripts/reencode.sh
@@ -1,8 +1,12 @@
 #!/usr/bin/bash
 
-echo "src ${1} dst ${2} duration ${3}"
+#echo "src ${1} dst ${2} duration ${3}"
 
 #reencode
-ffmpeg -i /var/www/streaming/rec/dispenser128-1727213365.flv -profile:v high -movflags +write_colr -crf 20 -c:v h264 -t 15 -y -colorspace 1 -color_primaries 1 -color_trc 1 -bsf:v h264_metadata=video_full_range_flag=0 -b:a 192k /tmp/test.mov
+#ffmpeg -i /var/www/streaming/rec/dispenser128-1727213365.flv -profile:v high -movflags +write_colr -crf 20 -c:v h264 -t 15 -y -colorspace 1 -color_primaries 1 -color_trc 1 -bsf:v h264_metadata=video_full_range_flag=0 -b:a 192k /tmp/test.mov
 #copy
-ffmpeg -i /var/www/streaming/rec/dispenser128-1727213365.flv -movflags +write_colr -crf 20 -c:v copy -t 15 -y -colorspace 1 -color_primaries 1 -color_trc 1 -bsf:v h264_metadata=video_full_range_flag=0 -b:a 192k /tmp/test.mov
+#ffmpeg -i /var/www/streaming/rec/dispenser128-1727213365.flv -movflags +write_colr -crf 20 -c:v copy -t 15 -y -colorspace 1 -color_primaries 1 -color_trc 1 -bsf:v h264_metadata=video_full_range_flag=0 -b:a 192k /tmp/reencoded.mov
+
+
+#ffmpeg -i /var/www/rec/dispenser128.flv -movflags +write_colr -c:v copy -c:a copy -t 15 -y -colorspace 1 -color_primaries 1 -color_trc 1 /tmp/send.mov
+ffmpeg -i "${1}" -movflags +write_colr -c:v copy -c:a copy -t 15 -y -r 30 -colorspace 1 -color_primaries 1 -color_trc 1 -bsf:v h264_metadata=video_full_range_flag=1 /tmp/send.mov
