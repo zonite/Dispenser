8c76b057a7b200fd6a123b6b6feb6f5d6cb2cc39
Matti Nykyri <zonite@iki.fi>
2024-09-26 16:41:56 +0300

    rtmp rec

--- daemon/skripts/startRec.sh
+++ daemon/skripts/startRec.sh
@@ -2,4 +2,5 @@
 
 echo "app ${1} name ${2}"
 
-wget "https://127.0.0.1:80/control/record/start?app=restream&name=dispenser128&rec=rec"
+#wget "https://127.0.0.1:80/control/record/start?app=restream&name=dispenser128&rec=rec"
+wget "https://127.0.0.1:80/control/record/start?app=${1}&name=${2}&rec=rec"
