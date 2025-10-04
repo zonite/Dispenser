bb580033c6630727902aa1e44e24f928a4e65cc8
Matti Nykyri <zonite@iki.fi>
2025-10-04 15:42:28 +0300

    mods

--- include/dispenser.h
+++ include/dispenser.h
@@ -8,7 +8,7 @@
 #define DOMAIN "nykyri.eu"
 #define APPNAME "Dispenser"
 
-#define DAEMON_NAME "DispenserD"
+#define DAEMON_NAME "dispenserd"
 #define DAEMON_VER "0.1"
 
 #define SEC_TO_MSEC(A) ((A) * 1000)
